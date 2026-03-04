610eb0155103a36cd09592d408546100a1b4ac16
Elfin1422 <raborarfrentdhieniel@gmail.com>
2026-03-04 20:30:39 +0800

    routes update

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,47 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\StockEntryController;
+use App\Http\Controllers\SupplierController;
+use App\Models\Product;
+use App\Models\StockEntry;
+use App\Models\Supplier;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
-    return view('welcome');
-});
+    $totalProducts = Product::count();
+    $totalSuppliers = Supplier::count();
+    $totalStockEntries = StockEntry::count();
+    $totalStockDelivered = StockEntry::sum('quantity');
+    
+    $recentStockEntries = StockEntry::with(['product', 'supplier'])
+        ->latest()
+        ->take(5)
+        ->get();
+    
+    $lowStockProducts = Product::where('current_stock', '<=', 10)
+        ->orderBy('current_stock')
+        ->take(5)
+        ->get();
+
+    return view('home', compact(
+        'totalProducts',
+        'totalSuppliers',
+        'totalStockEntries',
+        'totalStockDelivered',
+        'recentStockEntries',
+        'lowStockProducts'
+    ));
+})->name('home');
+
+
+Route::resource('products', ProductController::class);
+
+Route::resource('suppliers', SupplierController::class);
+
+Route::resource('stock_entries', StockEntryController::class);
+
 
 use App\Http\Controllers\PostController;
 
